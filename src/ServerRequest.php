f2b13ccacd5bbb1f67f9fb7b6854d5be05e198ab
pedro <pedro@phower.com>
2016-09-19 16:15:55 +0100

    Completed ServerRequest

--- src/ServerRequest.php
+++ src/ServerRequest.php
@@ -11,9 +11,9 @@
 
 namespace Phower\Http;
 
+use Phower\Http\Exception;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamInterface;
-use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\UploadedFileInterface;
 
 /**
  * Representation of an incoming, server-side HTTP request.
@@ -58,328 +58,58 @@ use Psr\Http\Message\UriInterface;
 class ServerRequest implements ServerRequestInterface
 {
 
-    /**
-     * Retrieves the HTTP protocol version as a string.
-     *
-     * The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
-     *
-     * @return string HTTP protocol version.
-     */
-    public function getProtocolVersion()
-    {
-
-    }
+    use MessageTrait,
+        RequestTrait;
 
     /**
-     * Return an instance with the specified HTTP protocol version.
-     *
-     * The version string MUST contain only the HTTP version number (e.g.,
-     * "1.1", "1.0").
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new protocol version.
-     *
-     * @param  string $version HTTP protocol version
-     * @return self
+     * @var array
      */
-    public function withProtocolVersion($version)
-    {
-
-    }
+    private $attributes;
 
     /**
-     * Retrieves all message header values.
-     *
-     * The keys represent the header name as it will be sent over the wire, and
-     * each value is an array of strings associated with the header.
-     *
-     *     // Represent the headers as a string
-     *     foreach ($message->getHeaders() as $name => $values) {
-     *         echo $name . ": " . implode(", ", $values){}
-     *     }
-     *
-     *     // Emit headers iteratively:
-     *     foreach ($message->getHeaders() as $name => $values) {
-     *         foreach ($values as $value) {
-     *             header(sprintf('%s: %s', $name, $value), false){}
-     *         }
-     *     }
-     *
-     * While header names are not case-sensitive, getHeaders() will preserve the
-     * exact case in which headers were originally specified.
-     *
-     * @return array Returns an associative array of the message's headers. Each
-     *     key MUST be a header name, and each value MUST be an array of strings
-     *     for that header.
+     * @var array
      */
-    public function getHeaders()
-    {
-
-    }
+    private $cookieParams;
 
     /**
-     * Checks if a header exists by the given case-insensitive name.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return bool Returns true if any header names match the given header
-     *     name using a case-insensitive string comparison. Returns false if
-     *     no matching header name is found in the message.
+     * @var array
      */
-    public function hasHeader($name)
-    {
-
-    }
+    private $parsedBody;
 
     /**
-     * Retrieves a message header value by the given case-insensitive name.
-     *
-     * This method returns an array of all the header values of the given
-     * case-insensitive header name.
-     *
-     * If the header does not appear in the message, this method MUST return an
-     * empty array.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return string[] An array of string values as provided for the given
-     *    header. If the header does not appear in the message, this method MUST
-     *    return an empty array.
+     * @var array
      */
-    public function getHeader($name)
-    {
-
-    }
+    private $queryParams;
 
     /**
-     * Retrieves a comma-separated string of the values for a single header.
-     *
-     * This method returns all of the header values of the given
-     * case-insensitive header name as a string concatenated together using
-     * a comma.
-     *
-     * NOTE: Not all header values may be appropriately represented using
-     * comma concatenation. For such headers, use getHeader() instead
-     * and supply your own delimiter when concatenating.
-     *
-     * If the header does not appear in the message, this method MUST return
-     * an empty string.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return string A string of values as provided for the given header
-     *    concatenated together using a comma. If the header does not appear in
-     *    the message, this method MUST return an empty string.
+     * @var array
      */
-    public function getHeaderLine($name)
-    {
-
-    }
+    private $serverParams;
 
     /**
-     * Return an instance with the provided value replacing the specified header.
-     *
-     * While header names are case-insensitive, the casing of the header will
-     * be preserved by this function, and returned from getHeaders().
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new and/or updated header and value.
-     *
-     * @param  string          $name  Case-insensitive header field name.
-     * @param  string|string[] $value Header value(s).
-     * @return self
-     * @throws \InvalidArgumentException for invalid header names or values.
+     * @var array
      */
-    public function withHeader($name, $value)
-    {
-
-    }
+    private $uploadedFiles;
 
     /**
-     * Return an instance with the specified header appended with the given value.
+     * Create new server request
      *
-     * Existing values for the specified header will be maintained. The new
-     * value(s) will be appended to the existing list. If the header did not
-     * exist previously, it will be added.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new header and/or value.
-     *
-     * @param  string          $name  Case-insensitive header field name to add.
-     * @param  string|string[] $value Header value(s).
-     * @return self
-     * @throws \InvalidArgumentException for invalid header names or values.
+     * @param array $server Server parameters, typically from $_SERVER
+     * @param array $files Upload file information, a tree of UploadedFiles
+     * @param \Psr\Http\Message\UriInterface|string $uri
+     * @param null|string $method HTTP method for the request, if any.
+     * @param \Psr\Http\Message\StreamInterface|string $body
+     * @param array $headers Headers for the message, if any.
+     * @throws InvalidArgumentException for any invalid value.
      */
-    public function withAddedHeader($name, $value)
+    public function __construct(array $server = [], array $files = [], $uri = '/', $method = 'GET', $body = 'php://input', array $headers = [])
     {
+        $this->validateUploadedFiles($files);
 
-    }
+        $this->init($uri, $body, $body, $headers);
 
-    /**
-     * Return an instance without the specified header.
-     *
-     * Header resolution MUST be done without case-sensitivity.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that removes
-     * the named header.
-     *
-     * @param  string $name Case-insensitive header field name to remove.
-     * @return self
-     */
-    public function withoutHeader($name)
-    {
-
-    }
-
-    /**
-     * Gets the body of the message.
-     *
-     * @return StreamInterface Returns the body as a stream.
-     */
-    public function getBody()
-    {
-
-    }
-
-    /**
-     * Return an instance with the specified message body.
-     *
-     * The body MUST be a StreamInterface object.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return a new instance that has the
-     * new body stream.
-     *
-     * @param  StreamInterface $body Body.
-     * @return self
-     * @throws \InvalidArgumentException When the body is not valid.
-     */
-    public function withBody(StreamInterface $body)
-    {
-
-    }
-
-    /**
-     * Retrieves the message's request target.
-     *
-     * Retrieves the message's request-target either as it will appear (for
-     * clients), as it appeared at request (for servers), or as it was
-     * specified for the instance (see withRequestTarget()).
-     *
-     * In most cases, this will be the origin-form of the composed URI,
-     * unless a value was provided to the concrete implementation (see
-     * withRequestTarget() below).
-     *
-     * If no URI is available, and no request-target has been specifically
-     * provided, this method MUST return the string "/".
-     *
-     * @return string
-     */
-    public function getRequestTarget()
-    {
-
-    }
-
-    /**
-     * Return an instance with the specific request-target.
-     *
-     * If the request needs a non-origin-form request-target — e.g., for
-     * specifying an absolute-form, authority-form, or asterisk-form —
-     * this method may be used to create an instance with the specified
-     * request-target, verbatim.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * changed request target.
-     *
-     * @link http://tools.ietf.org/html/rfc7230#section-2.7 (for the various
-     *     request-target forms allowed in request messages)
-     * @param mixed $requestTarget
-     * @return self
-     */
-    public function withRequestTarget($requestTarget)
-    {
-
-    }
-
-    /**
-     * Retrieves the HTTP method of the request.
-     *
-     * @return string Returns the request method.
-     */
-    public function getMethod()
-    {
-
-    }
-
-    /**
-     * Return an instance with the provided HTTP method.
-     *
-     * While HTTP method names are typically all uppercase characters, HTTP
-     * method names are case-sensitive and thus implementations SHOULD NOT
-     * modify the given string.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * changed request method.
-     *
-     * @param string $method Case-sensitive method.
-     * @return self
-     * @throws \InvalidArgumentException for invalid HTTP methods.
-     */
-    public function withMethod($method)
-    {
-
-    }
-
-    /**
-     * Retrieves the URI instance.
-     *
-     * This method MUST return a UriInterface instance.
-     *
-     * @link http://tools.ietf.org/html/rfc3986#section-4.3
-     * @return UriInterface Returns a UriInterface instance
-     *     representing the URI of the request.
-     */
-    public function getUri()
-    {
-
-    }
-
-    /**
-     * Returns an instance with the provided URI.
-     *
-     * This method MUST update the Host header of the returned request by
-     * default if the URI contains a host component. If the URI does not
-     * contain a host component, any pre-existing Host header MUST be carried
-     * over to the returned request.
-     *
-     * You can opt-in to preserving the original state of the Host header by
-     * setting `$preserveHost` to `true`. When `$preserveHost` is set to
-     * `true`, this method interacts with the Host header in the following ways:
-     *
-     * - If the the Host header is missing or empty, and the new URI contains
-     *   a host component, this method MUST update the Host header in the returned
-     *   request.
-     * - If the Host header is missing or empty, and the new URI does not contain a
-     *   host component, this method MUST NOT update the Host header in the returned
-     *   request.
-     * - If a Host header is present and non-empty, this method MUST NOT update
-     *   the Host header in the returned request.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new UriInterface instance.
-     *
-     * @link http://tools.ietf.org/html/rfc3986#section-4.3
-     * @param UriInterface $uri New request URI to use.
-     * @param bool $preserveHost Preserve the original state of the Host header.
-     * @return self
-     */
-    public function withUri(UriInterface $uri, $preserveHost = false)
-    {
-
+        $this->serverParams = $server;
+        $this->uploadedFiles = $files;
     }
 
     /**
@@ -393,7 +123,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getServerParams()
     {
-
+        return $this->serverParams;
     }
 
     /**
@@ -408,7 +138,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getCookieParams()
     {
-
+        return $this->cookieParams;
     }
 
     /**
@@ -426,11 +156,14 @@ class ServerRequest implements ServerRequestInterface
      * updated cookie values.
      *
      * @param  array $cookies Array of key/value pairs representing cookies.
-     * @return self
+     * @return \Phower\Http\ServerRequest
      */
     public function withCookieParams(array $cookies)
     {
+        $clone = clone $this;
+        $clone->cookieParams = $cookies;
 
+        return $clone;
     }
 
     /**
@@ -447,7 +180,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getQueryParams()
     {
-
+        return $this->queryParams;
     }
 
     /**
@@ -474,7 +207,10 @@ class ServerRequest implements ServerRequestInterface
      */
     public function withQueryParams(array $query)
     {
+        $clone = clone $this;
+        $clone->queryParams = $query;
 
+        return $clone;
     }
 
     /**
@@ -491,7 +227,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getUploadedFiles()
     {
-
+        return $this->uploadedFiles;
     }
 
     /**
@@ -507,7 +243,12 @@ class ServerRequest implements ServerRequestInterface
      */
     public function withUploadedFiles(array $uploadedFiles)
     {
+        $this->validateUploadedFiles($uploadedFiles);
 
+        $clone = clone $this;
+        $clone->uploadedFiles = $uploadedFiles;
+
+        return $clone;
     }
 
     /**
@@ -527,7 +268,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getParsedBody()
     {
-
+        return $this->parsedBody;
     }
 
     /**
@@ -560,7 +301,10 @@ class ServerRequest implements ServerRequestInterface
      */
     public function withParsedBody($data)
     {
+        $clone = clone $this;
+        $clone->parsedBody = $data;
 
+        return $clone;
     }
 
     /**
@@ -576,7 +320,7 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getAttributes()
     {
-
+        return $this->attributes;
     }
 
     /**
@@ -596,7 +340,11 @@ class ServerRequest implements ServerRequestInterface
      */
     public function getAttribute($name, $default = null)
     {
+        if (!array_key_exists($name, $this->attributes)) {
+            return $default;
+        }
 
+        return $this->attributes[$name];
     }
 
     /**
@@ -616,7 +364,10 @@ class ServerRequest implements ServerRequestInterface
      */
     public function withAttribute($name, $value)
     {
+        $clone = clone $this;
+        $clone->attributes[$name] = $value;
 
+        return $clone;
     }
 
     /**
@@ -635,6 +386,29 @@ class ServerRequest implements ServerRequestInterface
      */
     public function withoutAttribute($name)
     {
+        if (!isset($this->attributes[$name])) {
+            return clone $this;
+        }
 
+        $clone = clone $this;
+        unset($clone->attributes[$name]);
+
+        return $clone;
     }
+
+    private function validateUploadedFiles(array $uploadedFiles)
+    {
+        foreach ($uploadedFiles as $file) {
+            if (is_array($file)) {
+                $this->validateUploadedFiles($file);
+                continue;
+            }
+
+            if (!$file instanceof UploadedFileInterface) {
+                $message = 'Invalid file in uploaded files structure';
+                throw new Exception\InvalidArgumentException($message);
+            }
+        }
+    }
+
 }
