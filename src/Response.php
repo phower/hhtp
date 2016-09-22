f30aa21f66fe8d47f13a9fb54934b9c18f8526c4
pedro <pedro@phower.com>
2016-09-22 11:58:56 +0100

    Added tests for Response

--- src/Response.php
+++ src/Response.php
@@ -11,6 +11,7 @@
 
 namespace Phower\Http;
 
+use Phower\Http\Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 
@@ -34,206 +35,108 @@ use Psr\Http\Message\StreamInterface;
 class Response implements ResponseInterface
 {
 
-    /**
-     * Retrieves the HTTP protocol version as a string.
-     *
-     * The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
-     *
-     * @return string HTTP protocol version.
-     */
-    public function getProtocolVersion()
-    {
-
-    }
+    use MessageTrait;
 
     /**
-     * Return an instance with the specified HTTP protocol version.
+     * Map of standard HTTP status code/reason phrases
      *
-     * The version string MUST contain only the HTTP version number (e.g.,
-     * "1.1", "1.0").
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new protocol version.
-     *
-     * @param  string $version HTTP protocol version
-     * @return self
+     * @link https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+     * @var array
      */
-    public function withProtocolVersion($version)
-    {
-
-    }
+    private $phrases = array(
+        // INFORMATIONAL CODES
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        // SUCCESS CODES
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        // REDIRECTION CODES
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy', // Deprecated
+        307 => 'Temporary Redirect',
+        // CLIENT ERROR
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        // SERVER ERROR
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required',
+    );
 
     /**
-     * Retrieves all message header values.
-     *
-     * The keys represent the header name as it will be sent over the wire, and
-     * each value is an array of strings associated with the header.
-     *
-     *     // Represent the headers as a string
-     *     foreach ($message->getHeaders() as $name => $values) {
-     *         echo $name . ": " . implode(", ", $values){}
-     *     }
-     *
-     *     // Emit headers iteratively:
-     *     foreach ($message->getHeaders() as $name => $values) {
-     *         foreach ($values as $value) {
-     *             header(sprintf('%s: %s', $name, $value), false){}
-     *         }
-     *     }
-     *
-     * While header names are not case-sensitive, getHeaders() will preserve the
-     * exact case in which headers were originally specified.
-     *
-     * @return array Returns an associative array of the message's headers. Each
-     *     key MUST be a header name, and each value MUST be an array of strings
-     *     for that header.
+     * @var string
      */
-    public function getHeaders()
-    {
-
-    }
+    private $reasonPhrase = '';
 
     /**
-     * Checks if a header exists by the given case-insensitive name.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return bool Returns true if any header names match the given header
-     *     name using a case-insensitive string comparison. Returns false if
-     *     no matching header name is found in the message.
+     * @var int
      */
-    public function hasHeader($name)
+    private $statusCode = 200;
+
+    public function __construct($body = 'php://memory', $status = 200, array $headers = [])
     {
+        if (!is_string($body) && !is_resource($body) && !$body instanceof StreamInterface) {
+            $message = 'Body must be a string stream resource identifier, '
+                    . 'an actual stream resource, '
+                    . 'or a Psr\Http\Message\StreamInterface implementation';
+            throw new Exception\InvalidArgumentException($message);
+        }
 
-    }
+        if (null !== $status) {
+            $this->validateStatus($status);
+        }
 
-    /**
-     * Retrieves a message header value by the given case-insensitive name.
-     *
-     * This method returns an array of all the header values of the given
-     * case-insensitive header name.
-     *
-     * If the header does not appear in the message, this method MUST return an
-     * empty array.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return string[] An array of string values as provided for the given
-     *    header. If the header does not appear in the message, this method MUST
-     *    return an empty array.
-     */
-    public function getHeader($name)
-    {
+        $this->stream = ($body instanceof StreamInterface) ? $body : new Stream($body, 'wb+');
+        $this->statusCode = $status ? (int) $status : 200;
 
-    }
-
-    /**
-     * Retrieves a comma-separated string of the values for a single header.
-     *
-     * This method returns all of the header values of the given
-     * case-insensitive header name as a string concatenated together using
-     * a comma.
-     *
-     * NOTE: Not all header values may be appropriately represented using
-     * comma concatenation. For such headers, use getHeader() instead
-     * and supply your own delimiter when concatenating.
-     *
-     * If the header does not appear in the message, this method MUST return
-     * an empty string.
-     *
-     * @param  string $name Case-insensitive header field name.
-     * @return string A string of values as provided for the given header
-     *    concatenated together using a comma. If the header does not appear in
-     *    the message, this method MUST return an empty string.
-     */
-    public function getHeaderLine($name)
-    {
-
-    }
-
-    /**
-     * Return an instance with the provided value replacing the specified header.
-     *
-     * While header names are case-insensitive, the casing of the header will
-     * be preserved by this function, and returned from getHeaders().
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new and/or updated header and value.
-     *
-     * @param  string          $name  Case-insensitive header field name.
-     * @param  string|string[] $value Header value(s).
-     * @return self
-     * @throws \InvalidArgumentException for invalid header names or values.
-     */
-    public function withHeader($name, $value)
-    {
-
-    }
-
-    /**
-     * Return an instance with the specified header appended with the given value.
-     *
-     * Existing values for the specified header will be maintained. The new
-     * value(s) will be appended to the existing list. If the header did not
-     * exist previously, it will be added.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * new header and/or value.
-     *
-     * @param  string          $name  Case-insensitive header field name to add.
-     * @param  string|string[] $value Header value(s).
-     * @return self
-     * @throws \InvalidArgumentException for invalid header names or values.
-     */
-    public function withAddedHeader($name, $value)
-    {
-
-    }
-
-    /**
-     * Return an instance without the specified header.
-     *
-     * Header resolution MUST be done without case-sensitivity.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that removes
-     * the named header.
-     *
-     * @param  string $name Case-insensitive header field name to remove.
-     * @return self
-     */
-    public function withoutHeader($name)
-    {
-
-    }
-
-    /**
-     * Gets the body of the message.
-     *
-     * @return StreamInterface Returns the body as a stream.
-     */
-    public function getBody()
-    {
-
-    }
-
-    /**
-     * Return an instance with the specified message body.
-     *
-     * The body MUST be a StreamInterface object.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return a new instance that has the
-     * new body stream.
-     *
-     * @param  StreamInterface $body Body.
-     * @return self
-     * @throws \InvalidArgumentException When the body is not valid.
-     */
-    public function withBody(StreamInterface $body)
-    {
-
+        list($this->headerNames, $headers) = $this->filterHeaders($headers);
+        $this->assertHeaders($headers);
+        $this->headers = $headers;
     }
 
     /**
@@ -246,7 +149,7 @@ class Response implements ResponseInterface
      */
     public function getStatusCode()
     {
-
+        return $this->statusCode;
     }
 
     /**
@@ -266,12 +169,18 @@ class Response implements ResponseInterface
      * @param  string $reasonPhrase The reason phrase to use with the
      *     provided status code; if none is provided, implementations MAY
      *     use the defaults as suggested in the HTTP specification.
-     * @return self
+     * @return \Phower\Http\Response
      * @throws \InvalidArgumentException For invalid status code arguments.
      */
     public function withStatus($code, $reasonPhrase = '')
     {
+        $this->validateStatus($code);
 
+        $clone = clone $this;
+        $clone->statusCode = (int) $code;
+        $clone->reasonPhrase = $reasonPhrase;
+
+        return $clone;
     }
 
     /**
@@ -289,6 +198,39 @@ class Response implements ResponseInterface
      */
     public function getReasonPhrase()
     {
-
+        if (!$this->reasonPhrase && isset($this->phrases[$this->statusCode])) {
+            $this->reasonPhrase = $this->phrases[$this->statusCode];
+        }
+        
+        return $this->reasonPhrase;
     }
+
+    /**
+     * Validate a status code.
+     * 
+     * @param int $code
+     * @throws Exception\InvalidArgumentException
+     */
+    private function validateStatus($code)
+    {
+        if (!is_numeric($code) || is_float($code) || $code < 100 || $code >= 600) {
+            $message = sprintf('Invalid status code "%s"; must be an integer between 100 and 599, inclusive', is_scalar($code) ? $code : gettype($code));
+            throw new Exception\InvalidArgumentException($message);
+        }
+    }
+
+    /**
+     * Assert header names and values are valid
+     *
+     * @param array $headers
+     * @return void
+     */
+    private function assertHeaders(array $headers)
+    {
+        foreach ($headers as $name => $headerValues) {
+            HeaderSecurity::assertValidName($name);
+            array_walk($headerValues, __NAMESPACE__ . '\HeaderSecurity::assertValid');
+        }
+    }
+
 }
--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -21,10 +21,63 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
 
     public function testClassImplementsPsr7ResponseInterface()
     {
-        $message = $this->getMockBuilder(\Phower\Http\Response::class)
-                ->disableOriginalConstructor()
-                ->getMock();
-
-        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $message);
+        $response = $this->createMock(\Phower\Http\Response::class);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $response);
     }
+
+    /**
+     * @dataProvider constructProvider
+     */
+    public function testConstructCanInstantiateObject($body = 'php://memory', $status = 200, array $headers = [])
+    {
+        $response = new \Phower\Http\Response($body, $status, $headers);
+
+        $this->assertAttributeInstanceOf(\Psr\Http\Message\StreamInterface::class, 'stream', $response);
+        $this->assertAttributeEquals($status, 'statusCode', $response);
+        $this->assertAttributeEquals($status, 'statusCode', $response);
+        $this->assertAttributeInternalType('array', 'headers', $response);
+        $this->assertAttributeCount(count($headers), 'headers', $response);
+    }
+
+    public function constructProvider()
+    {
+        $body = $this->createMock(\Psr\Http\Message\StreamInterface::class);
+
+        return [
+            [],
+            ['php://memory'],
+            [$body],
+            [$body, 304],
+            [$body, 200, []],
+            [$body, 200, ['content-type' => 'text/html']],
+        ];
+    }
+
+    public function testConstructRaisesExceptionWhenBodyIsNotValid()
+    {
+        $this->setExpectedException(\Phower\Http\Exception\InvalidArgumentException::class);
+        $response = new \Phower\Http\Response([]);
+    }
+
+    public function testValidateStatusRaisesExceptionWhenStatusCodeIsNotValid()
+    {
+        $this->setExpectedException(\Phower\Http\Exception\InvalidArgumentException::class);
+        $response = new \Phower\Http\Response('php://memory', 'abc');
+    }
+
+    public function testWithStatusReturnsClonedResponseWithGivenStatusCode()
+    {
+        $response = new \Phower\Http\Response();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('OK', $response->getReasonPhrase());
+
+        $code = 333;
+        $reasonPhrase = 'Some phrase';
+
+        $clone = $response->withStatus($code, $reasonPhrase);
+        $this->assertNotSame($clone, $response);
+        $this->assertAttributeEquals($code, 'statusCode', $clone);
+        $this->assertAttributeEquals($reasonPhrase, 'reasonPhrase', $clone);
+    }
+
 }
